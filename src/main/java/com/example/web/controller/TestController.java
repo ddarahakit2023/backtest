db769eb70e22bf68ae8fd9c4babaab47928d7ba4
ddarahakit2023 <ddarahakit2023@gmail.com>
2023-09-13 13:04:21 +0900

    test

---
+++ src/main/java/com/example/web/controller/TestController.java
@@ -0,0 +1,16 @@
+package com.example.web.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @GetMapping("/test")
+    public ResponseEntity<Object> test(){
+        return ResponseEntity.ok().body("test");
+    }
+}
