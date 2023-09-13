bbf485a11ee44d9cd4189ed9ceab02e9ee5bbfc7
ddarahakit2023 <ddarahakit2023@gmail.com>
2023-09-13 14:09:47 +0900

    test

--- src/main/java/com/example/web/controller/TestController.java
+++ src/main/java/com/example/web/controller/TestController.java
@@ -9,8 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/test")
 public class TestController {
 
-    @GetMapping("/abcd")
+    @GetMapping("/test")
     public ResponseEntity<Object> test(){
-        return ResponseEntity.ok().body("zzzzz");
+        return ResponseEntity.ok().body("test");
     }
 }
